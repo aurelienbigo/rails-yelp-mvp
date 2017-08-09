85f8cf0d2d473cc2ed53da008e253bf4a37eed0a
aurelien bigo <aurelien.bigo@gmail.com>
2017-08-09 15:41:22 +0200

    yelp app working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   resources :restaurants do
     resources :reviews, only: [ :new, :create ]
   end
 
+  namespace :admin do
+    resources :restaurants, only: [:index, :create, :update, :delete]
+  end
 
+  root to: 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
